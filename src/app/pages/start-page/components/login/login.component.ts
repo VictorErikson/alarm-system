943dd6dbac51a02cf9b9cb951c6af520175c318e
VictorErikson <victoreriksson_93@hotmail.com>
2025-07-09 20:10:29 +0200

    worked with submitting form on login
    next up submitting form signing up

--- src/app/pages/start-page/components/login/login.component.ts
+++ src/app/pages/start-page/components/login/login.component.ts
@@ -1,11 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../../core/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  enteredEmail=signal('');
+  enteredPassword=signal('');
+  loginFailed = signal(false);
 
+  constructor(private auth: AuthService, private router: Router) {
+    effect(() => {
+    this.enteredEmail();
+    this.enteredPassword();
+    this.loginFailed.set(false);
+  });
+  }
+
+  login() {
+    const email = this.enteredEmail().trim();
+    const password = this.enteredPassword().trim();
+    const isValidEmail = /^[^\s@]+@[^\s@]+\.[a-zA-Z]+$/.test(email);
+
+    if (!email || password.length < 6 || !isValidEmail) {
+      this.loginFailed.set(true);
+      return;
+    }
+
+    this.auth.login(email, password).subscribe(success => {
+      if (success) {
+        this.router.navigate(['/home']);
+      } else {
+        this.loginFailed.set(true);
+      }
+    });
+  }
 }
--- src/app/pages/start-page/components/signup/signup.component.ts
+++ src/app/pages/start-page/components/signup/signup.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
+  enteredName = '';
+  enteredEmail = '';
+  enteredTel = '';
+  enteredPersonalNumber = '';
+  enteredPassword = '';
 
+  register() {
+    let userInfo = {
+      name: this.enteredName,
+      email: this.enteredEmail,
+      tel: this.enteredTel,
+      personalNumber: this.enteredPersonalNumber,
+      password: this.enteredPassword
+    }
+  }
 }
